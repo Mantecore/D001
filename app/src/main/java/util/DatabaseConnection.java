9972f8e32b8aa98bc9581b71c007ddcf0fe8551a
Mantecore <blizzbang@gmail.com>
2022-05-08 15:56:17 +0200

    Worked on database. Created SelectUserNames, SelectUserName, updateUserName and deleteUserName

--- app/src/main/java/util/DatabaseConnection.java
+++ app/src/main/java/util/DatabaseConnection.java
@@ -2,8 +2,12 @@ package util;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseConnection {
 
@@ -26,6 +30,51 @@ public class DatabaseConnection {
         values.put("height", height);
         values.put("calculatedBMI", calculatedBMI);
         database.insert(table, null, values);
+    }
+
+    public List<String> selectUsernames() {
+        String table = "UserNames";
+        Cursor result = database.query(table,null,null,null,null,null,null);
+        List<String> UserNames = new ArrayList<>();
+        if (result != null && result.moveToFirst()) {
+            do {
+                UserNames.add(result.getString(1));
+            } while (result.moveToNext());
+            result.close();
+        }
+        return UserNames;
+    }
+
+    public String userName(int id) {
+        String table = "UserNames";
+        String[] columns = {"name"};
+        String selection = "id = ?";
+        String[] selectionArgs = {Integer.toString(id)};
+        Cursor result = database.query(table,columns,selection,selectionArgs,null,null,null);
+        String bmiEntry = null;
+        if (result != null && result.moveToFirst()){
+            bmiEntry = result.getString(0);
+        }
+        return bmiEntry;
+    }
+
+
+    public void updateUsername (int id, String name) {
+        String table = "UserNames";
+        ContentValues values = new ContentValues();
+        values.put("name",name);
+        String selection = "id = ?";
+        String[] selectionArgs = {Integer.toString(id)};
+        database.update(table,values,selection,selectionArgs);
+    }
+
+    public void deleteUsername(int id) {
+        String table = "UserNames";
+        String selection = "id = ?";
+        String[] selectionArgs =  {
+                Integer.toString(id)
+        };
+        database.delete(table,selection,selectionArgs);
     }
 
     private static class DatabaseHelper extends SQLiteOpenHelper {
